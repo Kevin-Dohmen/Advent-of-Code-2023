40fd81aa13af117593ab319c3950c67522e4acca
Kevin Dohmen <81512056+Kevin-Dohmen@users.noreply.github.com>
2023-11-30 16:01:06 +0100

    Update makefolder.py

--- levels/makefolder.py
+++ levels/makefolder.py
@@ -1,22 +1,22 @@
 import os
 
-// define root dorectory
+# define root dorectory
 wd = os.getcwd()
 
 for i in range (1, 26):
-    // generate folder name
+    # generate folder name
     folderName = 'lvl_'
-    if I < 10:
+    if i < 10:
         folderName += '0'
     folderName += str(i)
 
-    // make directory and cd into it
+    # make directory and cd into it
     os.mkdir(folderName)
     os.chdir(folderName)
 
-    // make file
+    # make file
     f = open('score.txt', 'w')
     f.close()
 
-    // return to root directory
+    # return to root directory
     os.chdir(wd)
