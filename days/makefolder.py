84c6f788ebb85444310c6f802d834631206e9f7f
Kevin Dohmen <kevindohmen06@gmail.com>
2023-12-01 13:33:50 +0100

    improved file structure
    - added part1.py and part2.py to each day folder
    - added 2 extra day folders (range(0, 10) is 0 > 9) i forgor
    - used fstrings for feedback in makefolder.py

--- days/makefolder.py
+++ days/makefolder.py
@@ -6,9 +6,9 @@ wd = os.getcwd()
 folderAmnt = 25 # amount of folders to make (only supports below 100 for now)
 folderStartWith = 'day_' # will add number after
 
-filesToAdd = ['notes.txt', 'input.txt'] # files to add to each folder
+filesToAdd = ['notes.txt', 'input.txt', "part1.py", "part2.py"] # files to add to each folder
 
-for i in range (1, folderAmnt - 1):
+for i in range (1, folderAmnt + 1):
     # generate folder name
     folderName = folderStartWith
     if i < 10:
@@ -16,13 +16,21 @@ for i in range (1, folderAmnt - 1):
     folderName += str(i)
 
     # make directory and cd into it
-    os.mkdir(folderName)
+    if folderName not in os.listdir():
+        print(f"folder {folderName} does not exist: creating folder {folderName}")
+        os.mkdir(folderName)
+    else:
+        print(f"folder {folderName} already exists")
     os.chdir(folderName)
 
     # make file
     for item in filesToAdd:
-        f = open(item, 'w')
-        f.close()
+        if item not in os.listdir():
+            print(f"file {item} not in {folderName}")
+            f = open(item, 'w')
+            f.close()
+        else:
+            print(f"file {item} already exists in {folderName}")
 
     # return to root directory
     os.chdir(wd)
